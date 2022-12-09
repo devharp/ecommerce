d209b7ab31cc6d4a53d472a6a08cb4d522c057ec
devharp <hsingh.161297@gmail.com>
2022-12-09 12:46:24 +0530

    Added 'Add to cart' and 'Remove from cart' button

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,19 +1,22 @@
+import {useState} from 'react';
 import StarIcon from '@mui/icons-material/Star';
 import StarBorderIcon from '@mui/icons-material/StarBorder';
 import StarHalfIcon from '@mui/icons-material/StarHalf';
-
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import RemoveShoppingCartIcon from '@mui/icons-material/RemoveShoppingCart';
+import { Button } from '@mui/material';
 function ProductCard(props) {
     return (
         <div className="d-flex flex-column align-items-center m-4 p-5" style={{ boxShadow: '0 0 10px 5px rgb(0,0,0,0.1)', width: 'max-content' }}>
             <div className='d-flex' style={{ height: '13em', width: '13em', backgroundColor: 'red' }}>
-                
+
                 <img style={{ width: '100%' }} src={props.image} />
             </div>
             <div className="d-flex flex-column align-items-center mt-2">
                 <small className="mb-2" style={{ fontWeight: '00', color: 'rgb(150, 150, 150)' }}>{props.category}</small>
                 <h5 className="mb-2" style={{ width: '10em', textOverflow: 'ellipsis', overflow: 'hidden', whiteSpace: 'nowrap' }}>{props.title}</h5>
                 <div className="mb-2" style={{ fontWeight: '700' }}>{props.price}</div>
-                <div>
+                <div className='mb-2'>
                     {(parseInt(props.ratings.rate) >= 1) && <StarIcon sx={{ color: 'gold', fontSize: '1.25em' }} />}
                     {/* {((props.ratings.rate) > 0.5) && <StarHalfIcon sx={{ color: 'gold', fontSize: '1.25em' }} />} */}
                     {(props.ratings.rate < 0.5) && <StarBorderIcon sx={{ color: 'gold', fontSize: '1.25em' }} />}
@@ -35,6 +38,14 @@ function ProductCard(props) {
                     {(props.ratings.rate < 4.5) && <StarBorderIcon sx={{ color: 'gold', fontSize: '1.25em' }} />}
 
 
+                </div>
+                <div>
+                    {!props.bought && <Button onClick={props.onAddCartClicked} sx={{ 'bgcolor': 'rgb(50, 50, 50)', '&:hover': { bgcolor: 'rgb(20, 20, 20)' } }} size='small' variant="contained" startIcon={<AddShoppingCartIcon />}>
+                        Add To Cart
+                    </Button>}
+                    {props.bought && <Button onClick={props.removeFromCart} size='small' variant='outlined' sx={{ borderColor: 'rgb(50, 50, 50)', color: 'rgb(50,50,50)', '&:hover': { bgcolor: 'rgb(200, 200, 200, 0.1)', borderColor: 'rgb(20, 20, 20)', color: 'rgb(20,20,20)'} }} startIcon={<RemoveShoppingCartIcon/>}>
+                        Remove From Cart
+                    </Button>}
                 </div>
             </div>
         </div>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,23 +6,26 @@ function Home() {
     let once = false;
     const [searchtext, setSearchText] = useState('');
     const [products, setProducts] = useState([]);
-    async function fetchProducts(query) {
-        let link = 'http://localhost:3000/products';
-        if(query.length !== null){
-            link += `?search=${query}`
-        }
-        const res = await fetch(link)
-        const payload = await res.json();
-        setProducts(payload);
-    }
+
+    const [cartitems, setCartItems] = useState([]);
+    
     useEffect(() => {
         if (!once) {
-            fetchProducts();
+            fetchProducts(null);
             once = true;
         }
 
     }, []);
-
+    async function fetchProducts(query) {
+        let link = 'http://localhost:3000/products';
+        if(query !== null){
+            link += `?search=${query}`
+        }
+        const res = await fetch(link);
+        const payload = await res.json();
+        // console.log('recv: payload: ', payload);
+        setProducts(payload);
+    }
     async function userSearchQuery(event) {
         const query = event.target.value;
         if (query.length === 0) {
@@ -32,9 +35,33 @@ function Home() {
         fetchProducts(query)
     }
 
+    function onAddCartClicked(pid){
+        let found = false;
+        for (const e of cartitems) {
+            if(e === pid){
+                found = true;
+                break;
+            }
+        }
+        if(!found){
+            setCartItems([...cartitems, pid]);
+        }
+    }
+
+    function removeFromCart(pid){
+        let temp = [];
+        for (const e of cartitems) {
+            if(e !== pid){
+                temp.push(e);
+            }
+        }
+        setCartItems(temp);
+        console.log(pid);
+    }
+
     return (
         <div>
-            <NavBar searchQuery={userSearchQuery} ></NavBar>
+            <NavBar cartitems={String(cartitems.length)} searchQuery={userSearchQuery} ></NavBar>
             <h1 className='d-flex justify-content-center' style={{ marginTop: '3em' }}>Products</h1>
             <div className="d-flex px-4 justify-content-around flex-wrap">
                 {/* <ProductCard category='mens shopping' title='Product Name' price='$349.99' ratings={{ rate: 3.9, count: 120 }} /> */}
@@ -42,7 +69,7 @@ function Home() {
                     products.map((e, i) => {
                         return (
 
-                            <ProductCard key={'sdfdsf' + i} image={e.image} category={e.category} title={e.title} price={'$' + e.price} ratings={e.rating} />
+                            <ProductCard bought={cartitems.includes(e.pid)} removeFromCart={() => {removeFromCart(e.pid)}} onAddCartClicked={() => {onAddCartClicked(e.pid)}} key={'sdfdsf' + i} image={e.image} category={e.category} title={e.title} price={'$' + e.price} ratings={e.rating} />
                         )
                     })
                 }
